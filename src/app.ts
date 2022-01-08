3fd36121cfedc3e0daea4a9522bc8407f5ccdde5
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-08 18:02:38 +0100

    adding middlewares and types

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+
+import todoRoutes from "./routes/todos";
 
 const app = express();
+
+app.use(express.json());
+app.use("/todos", todoRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 
 app.listen(3000);
 
